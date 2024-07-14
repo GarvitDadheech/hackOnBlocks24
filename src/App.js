985347c3ccd1d21d1bba797f4c411620826167e5
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-07-14 20:36:55 +0530

    added page 3 basic

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Header from './components/LandingPage/Header/Header';
 import Main from './components/LandingPage/Main/Main';
 import Footer from './components/Footer/Footer';
-import Page2 from './components/SecurePage2/Page2'
+import Page2 from './components/Page2/Page2';
+import Page3 from './components/Page3/Page3';
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
       <Page2/>
+      <Page3/>
     </div>
   );
 }
---
+++ src/components/Page3/Page3.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Page3.css';
+const Page3 = () => {
+
+  return (
+        <div className='para'>
+            <div className='img'></div>
+            <div>
+                <div className='Para-info'>
+                    <h1 className="title">The Notification Framework for Developers and Product Teams</h1>
+                    <p className="subtitle">Fully extensible open source notifications infrastructure framework that empowers developers to provide an easy-to-use notifications platform to product teams.</p>
+                </div>
+            </div>
+        </div>
+  );
+};
+
+export default Page3;
