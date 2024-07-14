125402cc64a53277e7a58c93536a21d05d1ff4fc
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-07-14 20:47:35 +0530

    added page3 css

--- src/components/Page3/Page3.js
+++ src/components/Page3/Page3.js
@@ -5,11 +5,12 @@ const Page3 = () => {
   return (
         <div className='para'>
             <div className='img'></div>
-            <div>
+            <div className='content-holder'>
                 <div className='Para-info'>
                     <h1 className="title">The Notification Framework for Developers and Product Teams</h1>
                     <p className="subtitle">Fully extensible open source notifications infrastructure framework that empowers developers to provide an easy-to-use notifications platform to product teams.</p>
                 </div>
+                <button className="code-button">READ DOCS</button>
             </div>
         </div>
   );
