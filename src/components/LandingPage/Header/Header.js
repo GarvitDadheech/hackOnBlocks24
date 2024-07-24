15650eee9905aa0ddec091e81ed1cf35920c878f
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-07-24 23:03:46 +0530

    added demo link

--- src/components/LandingPage/Header/Header.js
+++ src/components/LandingPage/Header/Header.js
@@ -3,14 +3,14 @@ import './Header.css';
 
 const Header = () => {
   const getStarted = () => {
-    let startedbtn = document.querySelector('.App-header');
+    let startedbtn = document.querySelector('.features');
     startedbtn.scrollIntoView({behavior: 'smooth'});
   }
   return (
     <header className="header-container">
       <div className="logo">DevEXP</div>
       <nav className="nav">
-        <a href="#" className="nav-link">Demo</a>
+        <a href="https://youtu.be/uBIKsnWli2g?si=J9TMrMHACPlK7XAu" className="nav-link" target='blank'>Demo</a>
         <a href="https://github.com/GarvitDadheech/hackOnBlocks24" className="nav-link" target='blank'>Github</a>
         <a href="https://www.npmjs.com/package/payment-gateway-crypto" className="nav-link" target='blank'>Docs</a>
       </nav>
